6637d18f1c720275ec3216f2eed76bc5151bd1af
lhq8796 <lhq8796@outlook.com>
2021-08-28 22:17:33 +0800

    实现页面组件和路由

---
+++ src/views/Bookmarks.vue
@@ -0,0 +1,20 @@
+<template>
+  <TreeBookmarks :data="bookmarks" />
+</template>
+
+<script>
+import bookmarks from "@/assets/json/bookmarks";
+import TreeBookmarks from "@/components/TreeBookmarks";
+
+export default {
+  name: "Bookmarks",
+  components: {
+    TreeBookmarks,
+  },
+  data() {
+    return {
+      bookmarks,
+    };
+  },
+};
+</script>
