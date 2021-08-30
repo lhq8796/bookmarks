d8ded54b4d494bcbdbcab8af3cd84b40d1c25827
lhq8796 <lhq8796@outlook.com>
2021-08-30 23:36:49 +0800

    导航改为中文

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/bookmarks">Bookmarks</router-link>
+    <router-link to="/">主页</router-link> |
+    <router-link to="/about">关于</router-link> |
+    <router-link to="/bookmarks">书签</router-link>
   </div>
   <router-view />
 </template>
