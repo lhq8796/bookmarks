c3329d9978070738f6cc86060282e220a19a4304
lhq8796 <lhq8796@outlook.com>
2021-09-07 00:35:53 +0800

    修改书签页

--- src/components/TreeBookmarks.vue
+++ src/components/TreeBookmarks.vue
@@ -1,35 +1,27 @@
 <template>
-  <el-space v-if="data.type === 1" wrap size="large">
-    <el-card shadow="hover">
-      <template #header>
-        <div>
-          {{ data.name }}
-        </div>
-      </template>
-      <TreeBookmarks
-        v-for="item of data.children"
-        :key="item.name"
-        :data="item"
-      />
-    </el-card>
-  </el-space>
-  <div v-else-if="data.type === 2">
-    <el-link :href="data.href" target="_blank" rel="noopener noreferrer">
-      <span class="flex items-center">
-        <img v-if="data.icon" :src="data.icon" class="mr-2" />
+  <el-card shadow="hover">
+    <template #header>
+      <div>
         {{ data.name }}
-      </span>
-    </el-link>
-  </div>
+      </div>
+    </template>
+    <div v-for="item of data.children" :key="item.name">
+      <el-link :href="item.href" target="_blank" rel="noopener noreferrer">
+        <span class="flex items-center">
+          <img v-if="item.icon" :src="item.icon" class="mr-2" />
+          {{ item.name }}
+        </span>
+      </el-link>
+    </div>
+  </el-card>
 </template>
 
 <script>
-import { ElSpace, ElCard, ElLink } from "element-plus";
+import { ElCard, ElLink } from "element-plus";
 
 export default {
   name: "TreeBookmarks",
   components: {
-    ElSpace,
     ElCard,
     ElLink,
   },
