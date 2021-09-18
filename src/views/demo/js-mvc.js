6dbea0b5d0f61aabb7f96f31ecb659bf8a5d2a79
lhq8796 <lhq8796@outlook.com>
2021-09-18 23:28:37 +0800

    添加 js-mvc demo，完成 model 部分

---
+++ src/views/demo/js-mvc.js
@@ -0,0 +1,50 @@
+// 模型
+export class Model {
+  constructor() {
+    // The state of the model, an array of todo objects, prepopulated with some data
+
+    this.todos = [
+      { id: 1, text: "Run a marathon", complete: false },
+      { id: 2, text: "Plant a garden", complete: false },
+    ];
+  }
+
+  // Append a todo to the todos array
+  addTodo(todo) {
+    this.todos = [...this.todos, todo];
+  }
+
+  // Map through all todos, and replace the text of the todo width the speccified id
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map((todo) => {
+      todo.id === id
+        ? { id: todo.id, text: updatedText, complete: todo.complete }
+        : todo;
+    });
+  }
+
+  // Filter a todo out of the array by id
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  // Flip the complete boolean on the specified todo
+  toggleTodo(id) {
+    this.todos = this.todos.map((todo) => {
+      todo.id === id
+        ? { id: todo.id, text: todo.text, complete: !todo.complete }
+        : todo;
+    });
+  }
+}
+
+export class View {
+  constructor() {}
+}
+
+export class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+}
---
+++ src/views/demo/js-mvc.vue
@@ -0,0 +1,15 @@
+<template>123</template>
+
+<script>
+import { Model, View, Controller } from "./js-mvc.js";
+export default {
+  name: "JSMVC",
+  setup() {
+    const app = new Controller(new Model(), new View());
+    app.model.addTodo({ id: 3, text: "Take a nap", complete: false });
+    console.log(app.model.todos);
+  },
+};
+</script>
+
+<style></style>
