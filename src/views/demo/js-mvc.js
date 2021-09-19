69ba3b2f19d2464583fb0410b2d2909f969e1c9d
lhq8796 <lhq8796@outlook.com>
2021-09-19 17:16:00 +0800

    js-mvc demo 完成 view 部分

--- src/views/demo/js-mvc.js
+++ src/views/demo/js-mvc.js
@@ -39,7 +39,104 @@ export class Model {
 }
 
 export class View {
-  constructor() {}
+  constructor() {
+    // The root element
+    this.app = this.getElement("#root_js-mvc");
+
+    // The title of the app
+    this.title = this.createElement("h1");
+    this.title.textContent = "Todos";
+
+    // The form, with a [type="text"] input, and a submit button
+    this.form = this.createElement("form");
+
+    this.input = this.createElement("input");
+    this.input.type = "text";
+    this.input.placeholder = "Add todo";
+    this.input.name = "todo";
+
+    this.submitButton = this.createElement("button");
+    this.submitButton.textContent = "Submit";
+
+    // The visual representation of the todo list
+    this.todoList = this.createElement("ul", "todo-list");
+
+    // Append the input and submit button to the form
+    this.form.append(this.input, this.submitButton);
+
+    // Append the title, form, and todo list to the app
+    this.app.append(this.title, this.form, this.todoList);
+  }
+
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+
+    return element;
+  }
+
+  // Retrieve an element form the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector);
+
+    return element;
+  }
+
+  get todoText() {
+    return this.input.value;
+  }
+
+  resetInput() {
+    this.input.value = "";
+  }
+
+  displayTodos(todos) {
+    // Delete all nodes
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild);
+    }
+
+    // Show default message
+    if (todos.length === 0) {
+      const p = this.createElement("p");
+      p.textContent = "Nothing to do! Add a task?";
+      this.todoList.append(p);
+    } else {
+      todos.forEach((todo) => {
+        const li = this.createElement("li");
+        li.id = todo.id;
+
+        // Each todo item will have a checkbox you can toggle
+        const checkbox = this.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = todo.complete;
+
+        // The todo item will be in a contenteditable span
+        const span = this.createElement("span");
+        span.contentEditable = true;
+        span.classList.add("editable");
+
+        // If the todo is complete, it will have a strikethrough
+        if (todo.complete) {
+          const strike = this.createElement("s");
+          strike.textContent = todo.text;
+          span.append(strike);
+        } else {
+          // Otherwise just display the text
+          span.textContent = todo.text;
+        }
+
+        // The todos will also have a delete button
+        const deleteButton = this.createElement("button", "delete");
+        deleteButton.textContent = "Delete";
+        li.append(checkbox, span, deleteButton);
+
+        // Append nodes to the todo list
+        this.todoList.append(li);
+      });
+    }
+  }
 }
 
 export class Controller {
--- src/views/demo/js-mvc.vue
+++ src/views/demo/js-mvc.vue
@@ -1,13 +1,19 @@
-<template>123</template>
+<template>
+  <div id="root_js-mvc"></div>
+</template>
 
 <script>
+import "./js-mvc.scss";
+import { onMounted } from "@vue/runtime-core";
 import { Model, View, Controller } from "./js-mvc.js";
 export default {
   name: "JSMVC",
   setup() {
-    const app = new Controller(new Model(), new View());
-    app.model.addTodo({ id: 3, text: "Take a nap", complete: false });
-    console.log(app.model.todos);
+    onMounted(() => {
+      const app = new Controller(new Model(), new View());
+      app.model.addTodo({ id: 3, text: "Take a nap", complete: false });
+      console.log(app.model.todos);
+    });
   },
 };
 </script>
