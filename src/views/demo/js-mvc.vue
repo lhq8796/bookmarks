7b64fffbecf57cd622f47e57ed12524db61dd6e8
lhq8796 <lhq8796@outlook.com>
2021-09-20 17:36:32 +0800

    修改格式化规则

--- src/views/demo/js-mvc.vue
+++ src/views/demo/js-mvc.vue
@@ -3,19 +3,19 @@
 </template>
 
 <script>
-import "./js-mvc.scss";
-import { onMounted } from "@vue/runtime-core";
-import { Model, View, Controller } from "./js-mvc.js";
+import './js-mvc.scss'
+import { onMounted } from '@vue/runtime-core'
+import { Model, View, Controller } from './js-mvc.js'
 export default {
-  name: "JSMVC",
+  name: 'JSMVC',
   setup() {
     onMounted(() => {
-      const app = new Controller(new Model(), new View());
-      app.model.addTodo({ id: 3, text: "Take a nap", complete: false });
-      console.log(app.model.todos);
-    });
+      const app = new Controller(new Model(), new View())
+      app.model.addTodo({ id: 3, text: 'Take a nap', complete: false })
+      console.log(app.model.todos)
+    })
   },
-};
+}
 </script>
 
 <style></style>
